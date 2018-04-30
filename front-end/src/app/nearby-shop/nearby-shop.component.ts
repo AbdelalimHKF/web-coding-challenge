82aaa5e459f79dfbbde39c4aa302b34406145d4c
Abdelalim <abdelalim.hankafi@gmail.com>
2018-04-30 19:39:56 +0100

    Add like shop functionality

--- front-end/src/app/nearby-shop/nearby-shop.component.ts
+++ front-end/src/app/nearby-shop/nearby-shop.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit,Input } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-nearby-shop',
@@ -7,9 +8,19 @@ import { Component, OnInit,Input } from '@angular/core';
 })
 export class NearbyShopComponent implements OnInit {
 
-  constructor() { }
+  constructor( private http : HttpClient) { }
 
   ngOnInit() {
   }
   @Input() nearbyShop : any
+
+  url : String;
+
+  like(shop_id : number ){
+       //user/:user_id/like/shops/:shop_id
+       this.http.get("http://localhost:3000/v1/user/1/like/shops/"+shop_id)
+    .subscribe(data => {
+      console.log(data);
+    });
+  }
 }
