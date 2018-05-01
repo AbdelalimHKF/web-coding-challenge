b8e3ce71b36ad0e1c1f326b18b00b7edea570b8b
Abdelalim <abdelalim.hankafi@gmail.com>
2018-05-01 18:30:51 +0100

    Store current user id/email/token in a session_service when signing in

--- front-end/src/app/sign-in/sign-in.component.ts
+++ front-end/src/app/sign-in/sign-in.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AnonymousSubject } from 'rxjs/Subject';
+import 'rxjs/add/operator/map';
 import { User_credential } from '../user_credential';
+import { SessionService } from '../session.service';
 
 @Component({
   selector: 'app-sign-in',
@@ -10,18 +12,22 @@ import { User_credential } from '../user_credential';
 })
 export class SignInComponent implements OnInit {
 
-  constructor(private http :HttpClient) { }
+  
+  constructor(private http :HttpClient, private sessionService : SessionService) { }
 
   ngOnInit() {
   }
-  session : any;
+
   user_credential = new User_credential()
   sign_in(email : String ,password : String){
     this.user_credential.email=email;
     this.user_credential.password=password;
-    this.http.post("http://localhost:3000/v1/sessions",this.user_credential).
-    subscribe(data =>{
-      this.session=data;
+    this.http.post<SessionService>("http://localhost:3000/v1/sessions",this.user_credential)
+    .map(resp => resp)
+    .subscribe(data =>{
+      this.sessionService.id=data.id;
+      this.sessionService.email=data.email;
+      this.sessionService.authentication_token=data.authentication_token;
     });
   }
 
