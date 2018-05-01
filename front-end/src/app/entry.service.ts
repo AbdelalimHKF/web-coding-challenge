152f5736ec9f91d5afb7b83d4be4bbbb9ee12564
Abdelalim <abdelalim.hankafi@gmail.com>
2018-05-01 23:17:11 +0100

    Delegate sign-in action to entry-service)

--- front-end/src/app/entry.service.ts
+++ front-end/src/app/entry.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core';
+import { User_credential } from './user_credential';
+import { SessionService } from './session.service';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class EntryService {
 
-  constructor() { }
+  constructor(private http : HttpClient, private sessionService : SessionService ) { }
+
+  user_credential = new User_credential()
+  sign_in(email : String ,password : String){
+    this.user_credential.email=email;
+    this.user_credential.password=password;
+    this.http.post<SessionService>("http://localhost:3000/v1/sessions",this.user_credential)
+    .map(resp => resp)
+    .subscribe(data =>{
+      this.sessionService.id=data.id;
+      this.sessionService.email=data.email;
+      this.sessionService.authentication_token=data.authentication_token;
+    });
+  }
 
 }
--- front-end/src/app/sign-in/sign-in.component.ts
+++ front-end/src/app/sign-in/sign-in.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { AnonymousSubject } from 'rxjs/Subject';
-import 'rxjs/add/operator/map';
-import { User_credential } from '../user_credential';
 import { SessionService } from '../session.service';
+import { EntryService } from '../entry.service';
 
 @Component({
   selector: 'app-sign-in',
@@ -13,22 +10,12 @@ import { SessionService } from '../session.service';
 export class SignInComponent implements OnInit {
 
   
-  constructor(private http :HttpClient, private sessionService : SessionService) { }
+  constructor(private entryService : EntryService) { }
 
   ngOnInit() {
   }
 
-  user_credential = new User_credential()
   sign_in(email : String ,password : String){
-    this.user_credential.email=email;
-    this.user_credential.password=password;
-    this.http.post<SessionService>("http://localhost:3000/v1/sessions",this.user_credential)
-    .map(resp => resp)
-    .subscribe(data =>{
-      this.sessionService.id=data.id;
-      this.sessionService.email=data.email;
-      this.sessionService.authentication_token=data.authentication_token;
-    });
+    this.entryService.sign_in(email,password);
   }
-
 }
