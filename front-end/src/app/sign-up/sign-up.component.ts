f09a5fead85474a0dd488ff1bb7f6da542705600
Abdelalim <abdelalim.hankafi@gmail.com>
2018-05-01 14:47:11 +0100

    Add Sign up functionality

--- front-end/src/app/sign-up/sign-up.component.ts
+++ front-end/src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User_credential } from '../user_credential';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
 
   ngOnInit() {
   }
 
+  
+  user_credential = new User_credential()
+  sign_up(email : String ,password : String){
+    this.user_credential.email=email;
+    this.user_credential.password=password;
+    this.http.post("http://localhost:3000/v1/users",this.user_credential)
+     .subscribe(data =>{
+      console.log("user created",data);
+    });
+  }
 }
