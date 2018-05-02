fa91e11f023e19b10b3f8b2ccc9d55281a9ad462
Abdelalim <abdelalim.hankafi@gmail.com>
2018-05-02 20:16:45 +0100

    Change url  http methode  for actions  like,dislike and remove

--- front-end/src/app/action.service.ts
+++ front-end/src/app/action.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { SessionService } from './session.service';
 
 @Injectable()
 export class ActionService {
 
-  constructor(private http : HttpClient) { }
+  constructor(private http : HttpClient,private sessionService :SessionService ) { }
+
+  server_name : string ="http://localhost:3000"
+  url_users : string = "/v1/user/"+this.sessionService.id;
+  token: string ="?authentication_token="+this.sessionService.authentication_token;
 
   like(shop_id : number ){
     //user/:user_id/like/shops/:shop_id
-    this.http.get("http://localhost:3000/v1/user/1/like/shops/"+shop_id)
+    let url = this.server_name+this.url_users+"/like/shops/"+shop_id+this.token
+    this.http.get(url)
       .subscribe(data => {
         console.log(data);
       });
@@ -16,7 +22,8 @@ export class ActionService {
 
   dislike(shop_id : number ){
     //api : user/:user_id/dislike/shops/:shop_id
-    this.http.get("http://localhost:3000/v1/user/1/dislike/shops/"+shop_id)
+    let url = this.server_name+this.url_users+"/dislike/shops/"+shop_id+this.token
+    this.http.get(url)
       .subscribe(data => {
         console.log(data);
       });
@@ -24,7 +31,8 @@ export class ActionService {
 
   remove(shop_id : number ){
     //user/:user_id/like/shops/:shop_id
-    this.http.get("http://localhost:3000/v1/user/1/remove/shops/"+shop_id)
+    let url = this.server_name+this.url_users+"/remove/shops/"+shop_id+this.token
+    this.http.get(url)
       .subscribe(data => {
         console.log(data);
       });
