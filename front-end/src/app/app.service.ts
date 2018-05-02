06b6df295da297b3756d92b173325a79005ac587
Abdelalim <abdelalim.hankafi@gmail.com>
2018-05-02 03:36:28 +0100

    Change url for http methode to get nearby and preferred shops for the current user

--- front-end/src/app/app.service.ts
+++ front-end/src/app/app.service.ts
@@ -1,18 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+import { SessionService } from './session.service';
 
 @Injectable()
 export class AppService {
 
-  constructor(private http : HttpClient) { }
+  constructor(private http : HttpClient, private sessionService : SessionService) { }
+
+  url_users : string = "http://localhost:3000/v1/users/"+this.sessionService.id;
 
   getNearbyShops() : Observable <any> {
-    return this.http.get("http://localhost:3000/v1/users/1/nearby_shops");
+    return this.http.get(this.url_users+"/nearby_shops");
   }
 
   getPreferredShops(): Observable <any> {
-   return  this.http.get("http://localhost:3000/v1/users/1/preferred_shops");
+   return  this.http.get(this.url_users+"/preferred_shops");
   }
 
 }
