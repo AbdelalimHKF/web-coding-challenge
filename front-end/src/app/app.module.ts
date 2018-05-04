b9e743b531c9ea1983feb3088f783c2ca5feea87
Abdelalim <abdelalim.hankafi@gmail.com>
2018-05-04 17:51:12 +0100

    Add Material stye to buttons like, dislike and remove

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { SharedService } from './shared.service';
 import { FctService } from './fct.service';
 
 import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
@@ -40,7 +41,8 @@ import {MatCardModule} from '@angular/material/card';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [SessionService, ActionService, EntryService, AppService, AuthGuard, SharedService, FctService],
   bootstrap: [AppComponent]
