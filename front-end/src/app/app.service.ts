03669e6752a23770a0a09a75859be512800dcc00
Abdelalim <abdelalim.hankafi@gmail.com>
2018-05-06 07:52:27 +0100

    Add geo coordinates paramameters to url

--- front-end/src/app/app.service.ts
+++ front-end/src/app/app.service.ts
@@ -3,17 +3,22 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { SessionService } from './session.service';
 import 'rxjs/add/operator/map';
+import { LocationService } from './location.service';
 
 @Injectable()
 export class AppService {
 
-  constructor(private http : HttpClient, private sessionService : SessionService) { }
+  constructor(private http : HttpClient, 
+              private sessionService : SessionService,
+              private locationServive : LocationService ) { }
 
+  
   url_users : string = "http://localhost:3000/v1/users/"+this.sessionService.id;
   token: string ="?authentication_token="+this.sessionService.authentication_token;
+  coordinate_params : string = "&logitude=" +this.locationServive.longitude+"&latitude="+this.locationServive.latitude;
 
   getNearbyShops() : Observable <any> {
-    return this.http.get(this.url_users+"/nearby_shops"+this.token,{observe :'response'});
+    return this.http.get(this.url_users+"/nearby_shops"+this.token+this.coordinate_params,{observe :'response'});
   }
 
   getPreferredShops(): Observable <any> {
